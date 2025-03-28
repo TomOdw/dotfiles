f7705954a798a8bf2613c876166a60b6533f0ac8
Tom Christ <tom.christ.99@gmail.com>
2025-03-28 11:57:54 +0100

    Initial nvim commit

---
+++ .config/nvim/lua/mappings.lua
@@ -0,0 +1,17 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+
+-- tumux navigator override. 
+map("n", "<c-h>", "<cmd>TmuxNavigateLeft<cr>")
+map("n", "<c-j>", "<cmd>TmuxNavigateDown<cr>")
+map("n", "<c-k>", "<cmd>TmuxNavigateUp<cr>")
+map("n", "<c-l>", "<cmd>TmuxNavigateRight<cr>")
+map("n", "<c-\\>", "<cmd>TmuxNavigatePrevious<cr>")
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
---
+++ .config/nvim/lua/plugins/init.lua
@@ -0,0 +1,54 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  -- tmux navigator
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+      "TmuxNavigatorProcessList",
+    },
+    lazy = false,
+  },
+
+  {
+   "williamboman/mason.nvim",
+   opts = {
+    ensure_installed = {
+      "lua-language-server",
+      "stylua",
+      "html-lsp",
+      "css-lsp",
+      "prettier",
+      "pyre",
+      "clangd"
+    },
+   },
+  },
+
+{
+	"nvim-treesitter/nvim-treesitter",
+	opts = {
+		ensure_installed = {
+			"vim", "lua", "vimdoc",
+     "html", "css", "c", "cpp"
+		},
+	},
+},
+}
