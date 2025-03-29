eed204904bbfc26076b3d73433721cdaed52e114
Tom Christ <tom.christ.99@gmail.com>
2025-03-29 17:54:50 +0100

    Set some options and mappings... we will see

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,6 +1,33 @@
 require "nvchad.options"
 
--- add yours here!
-
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+vim.o.mouse = 'a' -- Enable mouse mode
+vim.o.clipboard = 'unnamedplus' -- Sync clipboard between OS and Neovim.
+vim.o.breakindent = true -- Enable break indent
+vim.o.undofile = true -- Save undo history
+vim.o.ignorecase = true -- Case-insensitive searching UNLESS \C or capital in search
+vim.o.smartcase = true -- smart case
+vim.o.backup = false -- creates a backup file
+vim.o.writebackup = false -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+vim.o.completeopt = 'menuone,noselect' -- Set completeopt to have a better completion experience
+vim.o.whichwrap = 'bs<>[]hl' -- which "horizontal" keys are allowed to travel to prev/next line
+vim.o.wrap = false -- display lines as one long line
+vim.o.linebreak = true -- companion to wrap don't split words
+vim.o.scrolloff = 4 -- minimal number of screen lines to keep above and below the cursor
+vim.o.sidescrolloff = 8 -- minimal number of screen columns either side of cursor if wrap is `false`
+vim.o.relativenumber = true -- set relative numbered lines
+vim.o.numberwidth = 4 -- set number column width to 2 {default 4}
+vim.o.shiftwidth = 3 -- the number of spaces inserted for each indentation
+vim.o.tabstop = 3 -- insert n spaces for a tab
+vim.o.softtabstop = 3 -- Number of spaces that a tab counts for while performing editing operations
+vim.o.expandtab = true -- convert tabs to spaces
+vim.o.cursorline = false -- highlight the current line
+vim.o.splitbelow = true -- force all horizontal splits to go below current window
+vim.o.splitright = true -- force all vertical splits to go to the right of current window
+vim.o.swapfile = false -- creates a swapfile
+vim.o.smartindent = true -- make indenting smarter again
+vim.o.backspace = 'indent,eol,start' -- allow backspace on
+vim.o.pumheight = 10 -- pop up menu height
+vim.o.conceallevel = 0 -- so that `` is visible in markdown files
+vim.o.fileencoding = 'utf-8' -- the encoding written to a file
+vim.o.cmdheight = 1 -- more space in the neovim command line for displaying messages
+vim.o.autoindent = true -- copy indent from current line when starting new one
